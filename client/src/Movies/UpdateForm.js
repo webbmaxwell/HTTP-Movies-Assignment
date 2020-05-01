7c795719e0b8f56c53c58dfd2ceb6824c0ec1c1c
webbmaxwell <webbwrites@gmail.com>
2020-04-30 21:10:46 -0600

    Added 'Update' button to form. Logs to the console but does not update the data on the page.

--- client/src/Movies/UpdateForm.js
+++ client/src/Movies/UpdateForm.js
@@ -16,6 +16,7 @@ class UpdateForm extends React.Component {
   }
 
   handleChange = e => {
+    console.log(this);
     this.setState({
       movie: {
         ...this.state.movie,
@@ -26,12 +27,15 @@ class UpdateForm extends React.Component {
 
   render() {
     return (
-      <form>
-        <input type="text" name="title" placeholder="title" />
-        <input type="text" name="director" placeholder="director" />
-        <input type="text" name="metascore" placeholder="metascore" />
-        <input type="text" name="stars" placeholder="stars" />
-      </form>
+      <div className="update-form">
+        <form>
+          <input type="text" name="title" placeholder="title" />
+          <input type="text" name="director" placeholder="director" />
+          <input type="text" name="metascore" placeholder="metascore" />
+          <input type="text" name="stars" placeholder="stars" />
+        </form>
+        <button type="submit" onClick={this.handleChange}>Update</button>
+      </div>
   )};
 };
 
